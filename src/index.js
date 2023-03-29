f257bbfc9fd5b88720223f3ea7313b2732722624
dennis kimani <denniskimani416@gmail.com>
2023-03-29 17:21:54 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const taskList = document.getElementById('tasks');
+  const taskInput = document.getElementById('newTaskDescription');
+  const submitButton = document.querySelector('input[type="submit"]');
+
+  submitButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    const taskText = taskInput.value;
+    const taskItem = document.createElement('li');
+    taskItem.textContent = taskText;
+    taskList.appendChild(taskItem);
+    taskInput.value = '';
+  });
 });
